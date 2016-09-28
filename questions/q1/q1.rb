6934aeb6793436cea7ab9e4bfdc3b821e59d4367
tooppoo <Ryouta@kuribayashiryouta-no-MacBook-Pro.local>
2016-09-29 00:33:05 +0900

    回文か否かの判定処理を、String自身に追加

--- questions/q1/q1.rb
+++ questions/q1/q1.rb
@@ -1,21 +1,19 @@
 # coding: utf-8
 
 module Q1
+  class ::String
+    def palindrom?
+      self == self.reverse
+    end
+  end
+
   module Lazy
     # Ruby依存の解法
     class << self
       def exec(num)
-        return false unless palindrom?(num, :binary)
-        return false unless palindrom?(num, :octal)
-        palindrom?(num, :decimal)
-      end
-
-      private
-      def palindrom?(num, ary)
-        ary_vals = {binary: 2, octal: 8, decimal: 10}
-
-        comberted = num.to_s(ary_vals[ary])
-        comberted == comberted.reverse
+        return false unless num.to_s(2).palindrom?
+        return false unless num.to_s(8).palindrom?
+        num.to_s.palindrom?
       end
     end
   end
@@ -23,6 +21,8 @@ module Q1
   module Primitive
     # 原理に基づいた解法
     class ::Fixnum
+      alias_method :to_decimal, :to_s
+
       def to_binary
         combert_to(principal_num: 2, base: self, char: [])
       end
@@ -40,9 +40,9 @@ module Q1
 
     class << self
       def exec(num)
-        return false unless num.to_binary.reverse == num.to_binary
-        return false unless num.to_octal.reverse == num.to_octal
-        num.to_s.reverse == num.to_s
+        return false unless num.to_binary.palindrom?
+        return false unless num.to_octal.palindrom?
+        num.to_decimal.palindrom?
       end
     end
   end
